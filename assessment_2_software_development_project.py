57b549354f1554134a44df39c995d6b87f8bca13
Ethan Andrei Rivera <ethanrivera0409@gmail.com>
2026-06-23 10:01:52 +1200

    I created the requisition system class to encapsulate all the initial methods. I also used multiple pass statements for each method for now because I will build up each method one by one.

---
+++ assessment_2_software_development_project.py
@@ -0,0 +1,18 @@
+class requisitionSystem: # Class to encapsulate methods and parameters for requistion system
+  def __init__(self): # Constructor Method
+    pass
+
+  def addRequistion(self): # Method to add a requisition entry
+    pass
+
+  def approveRequisition(self): # Method to approve requisition entry
+    pass
+
+  def respondRequisition(self): # Method to respond to requisition entry 
+    pass
+
+  def displayRequisition(self): # Method to display requistion information
+    pass
+
+  def requisition_statistics(self): # Method to display the statistics of the requistion system
+    pass
