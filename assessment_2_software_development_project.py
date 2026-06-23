66c8e742e289bdb768be38f074401539caf7cd94
Ethan Andrei Rivera <ethanrivera0409@gmail.com>
2026-06-24 09:45:19 +1200

    I have added the following into the program since creating the class and methods:
    - A private list named self.__requisitions within the constructor method to store the requisitions entries.
    
    - For the variables required in the requisition system, I used different techniques such as using input functions to ask the user for the date, staff ID, staff name, total amount the employee is requesting for their requisition entry.
    
    - The requisition ID will start at 10001 and will increment for every entry. I have yet to add the loop for it, I will add it soon with the upcoming commits.
    
    - I have added a dictionary named requisition to store the information of each entry. I have also implemented the append string method to push the req entry into the private list.
    
    - I added a print statement that confirms the req entry with its ID has been added to the system.

--- assessment_2_software_development_project.py
+++ assessment_2_software_development_project.py
@@ -1,9 +1,34 @@
 class requisitionSystem: # Class to encapsulate methods and parameters for requistion system
   def __init__(self): # Constructor Method
-    pass
+    self.__requisitions = []
 
   def addRequistion(self): # Method to add a requisition entry
-    pass
+
+    date = input("Enter the date: (DD/MM/YEAR)")
+
+    staff_id = input("Enter your 4 Digit Staff ID: ")
+
+    staff_name = input("Enter your name: ")
+
+    staff_name = staff_name.title
+
+    requisition_id = 10000 + 1
+
+    total = int(input("Enter your total amount the you would like to requisite: "))
+    
+    approval_reference_number = staff_id + requisition_id[2:]
+
+    requisition = {"Date": date,
+                   "Staff ID": staff_id,
+                   "Staff Name": staff_name,
+                   "Requisition ID": requisition_id,
+                   "Total": total,
+                   "Status": 'Pending',
+                   "Approval Reference Number": approval_reference_number}
+  
+    self.__requisitions.append(requisition)
+
+    print(f"Requisition {requisition_id} has been added to the system.")
 
   def approveRequisition(self): # Method to approve requisition entry
     pass
