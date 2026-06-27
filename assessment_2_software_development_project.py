dc04c09ad3220fbcc64c0a945058d65b8c639b31
Ethan Andrei Rivera <ethanrivera0409@gmail.com>
2026-06-27 17:35:45 +1200

    To make it easier for me to call the variables I need from different methods, I used the constructor method to declare the variables as parameters.
    I then used self. to initialize each parameter for each method that I used these parameters.
    
    I've added a simple if else statement in the approve requisition method
    
    If the total amount is less than 500, the requisition is approved. otherwise, it is not approved. Either way, this will overwrite the initial value of status "Pending."
    
    I also made the default value of the approval number as not available, this will change once a requisition entry's status is "approved."

--- assessment_2_software_development_project.py
+++ assessment_2_software_development_project.py
@@ -1,37 +1,45 @@
 class requisitionSystem: # Class to encapsulate methods and parameters for requistion system
-  def __init__(self): # Constructor Method
+  def __init__(self, date, staff_id, staff_name, requisition_id, total, approval_reference_number): # Constructor Method
     self.__requisitions = []
+    self.__approved_requisitions = []
     self.__item_list = []
     self.__price_list = []
 
+    self.date = date
+    self.staff_id = staff_id
+    self.staff_name = staff_name
+    self.requisition_id = requisition_id
+    self.total = total
+    self.approval_reference_number = approval_reference_number
+
   def addRequistion(self): # Method to add a requisition entry
 
-    date = input("Enter the date (DD/MM/YEAR): ") # Asks user to enter date
+    self.date = input("Enter the date (DD/MM/YEAR): ") # Asks user to enter date
 
-    staff_id = input("Enter your 4 Digit Staff ID: ") # Asks user to enter 4 digit staff ID
+    self.staff_id = input("Enter your 4 Digit Staff ID: ") # Asks user to enter 4 digit staff ID
 
-    staff_name = input("Enter your name: ").title # Asks user to enter full name, used title to capitalize the first letter of each section of the name
+    self.staff_name = input("Enter your name: ").title # Asks user to enter full name, used title to capitalize the first letter of each section of the name
 
-    requisition_id = 10000 + counter # Value for the requisition ID starts at 
+    self.requisition_id = 10000 + counter # Value for the requisition ID starts at 
 
     for requisition in self.__requisitions: #Per new requisition, the counter will increment by 1 to allow each requisition ID to be unique
       counter =+ 1
 
-    total = int(input("Enter your total amount the you would like to requisite: $"))
+    self.total = int(input("Enter your total amount the you would like to requisite: $"))
     
-    approval_reference_number = staff_id + requisition_id[-3:]
+    self.approval_reference_number = self.staff_id + self.requisition_id[-3:]
 
-    requisition = {"Date": date,
-                   "Staff_ID": staff_id,
-                   "Staff_Name": staff_name,
-                   "Requisition_ID": requisition_id,
-                   "Total": total,
+    requisition = {"Date": self.date,
+                   "Staff_ID": self.staff_id,
+                   "Staff_Name": self.staff_name,
+                   "Requisition_ID": self.requisition_id,
+                   "Total": self.total,
                    "Status": 'Pending',
-                   "Approval_Reference_Number": approval_reference_number}
+                   "Approval_Reference_Number": 'Not available'}
   
     self.__requisitions.append(requisition)
 
-    print(f"Requisition {requisition_id} has been added to the system.")
+    print(f"Requisition {self.requisition_id} has been added to the system.")
 
   def viewRequisition(self):
     if len(self.__requisitions) == 0:
@@ -58,7 +66,11 @@ class requisitionSystem: # Class to encapsulate methods and parameters for requi
         print("-------------------")
 
   def approveRequisition(self): # Method to check if the requisition entry is approved or not
-    pass
+    if self.total < 500:
+     self.status = "Approved"
+
+    else:
+      self.total = "Not Approved"
 
   def respondRequisition(self): # Method to respond to requisition entry by changing the status (pending to approved or not approved)
     pass
