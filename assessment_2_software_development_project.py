9e6a7632fc1bb810adf32dbe75b08ffb8f2753d5
Ethan Andrei Rivera <ethanrivera0409@gmail.com>
2026-06-27 14:49:01 +1200

    I've added multiple things to the program:
    Private lists to compile the items and prices that will be displayed later on. I will implement a way to get the total amount of the prices laer on, but for now I'll just input the total.
    
    I've added a counter that uses a for loop for the next requisition entry to have a unique req ID that will increment by 1.
    
    I changed the value of the index in the approve ref number to -3 for me to get the last three characters of the req ID.

--- assessment_2_software_development_project.py
+++ assessment_2_software_development_project.py
@@ -1,22 +1,25 @@
 class requisitionSystem: # Class to encapsulate methods and parameters for requistion system
   def __init__(self): # Constructor Method
     self.__requisitions = []
+    self.__item_list = []
+    self.__price_list = []
 
   def addRequistion(self): # Method to add a requisition entry
 
-    date = input("Enter the date: (DD/MM/YEAR)") # Asks user to enter date
+    date = input("Enter the date (DD/MM/YEAR): ") # Asks user to enter date
 
     staff_id = input("Enter your 4 Digit Staff ID: ") # Asks user to enter 4 digit staff ID
 
-    staff_name = input("Enter your name: ") # Asks user to enter full name
+    staff_name = input("Enter your name: ").title # Asks user to enter full name, used title to capitalize the first letter of each section of the name
 
-    staff_name = staff_name.title # Capitalize the first letter of each section of the name
+    requisition_id = 10000 + counter # Value for the requisition ID starts at 
 
-    requisition_id = 10000 + 1 # Value for the requisition ID starts at 
+    for requisition in self.__requisitions: #Per new requisition, the counter will increment by 1 to allow each requisition ID to be unique
+      counter =+ 1
 
-    total = int(input("Enter your total amount the you would like to requisite: "))
+    total = int(input("Enter your total amount the you would like to requisite: $"))
     
-    approval_reference_number = staff_id + requisition_id[2:]
+    approval_reference_number = staff_id + requisition_id[-3:]
 
     requisition = {"Date": date,
                    "Staff_ID": staff_id,
@@ -35,11 +38,15 @@ class requisitionSystem: # Class to encapsulate methods and parameters for requi
       print("No requisition entries found.")
 
     else:
+      
       print("-------------------")
+
       print("Requisition Requests")
+
       print("-------------------")
 
       for requisition in self.__requisitions:
+
         print(f"Date: {requisition["Date"]}")
         print(f"Staff ID: {requisition["Staff_ID"]}")
         print(f"Staff Name: {requisition["Staff_Name"]}")
@@ -48,10 +55,12 @@ class requisitionSystem: # Class to encapsulate methods and parameters for requi
         print(f"Status: {requisition["Status"]}")
         print(f"Approval Reference Number: {requisition["Approval_Reference_Number"]}")
 
-  def approveRequisition(self): # Method to approve requisition entry
+        print("-------------------")
+
+  def approveRequisition(self): # Method to check if the requisition entry is approved or not
     pass
 
-  def respondRequisition(self): # Method to respond to requisition entry 
+  def respondRequisition(self): # Method to respond to requisition entry by changing the status (pending to approved or not approved)
     pass
 
   def displayRequisition(self): # Method to display requistion information
