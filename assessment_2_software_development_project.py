cf3f28546d711941e83dede82b8378a21dbe4f5e
Ethan Andrei Rivera <ethanrivera0409@gmail.com>
2026-06-30 12:34:40 +1200

    The next error occured when I tried testing the add requisition method. The method would not proceed because the counter is within the class. Because of this, I need to embed the counter outside the class and also use global to access the counter variable within the addRequisition method.
    I also redid some code within the same method by removing the for loop, as I can just use the global counter and reuse it for other requisition entries.
    
    The requisitionDetails will be assigned to avaribale named details which will be used later.

--- assessment_2_software_development_project.py
+++ assessment_2_software_development_project.py
@@ -1,3 +1,5 @@
+counter = 10000
+
 class requisitionSystem: # Class to encapsulate methods and parameters for requistion system
   def __init__(self): # Constructor Method
 
@@ -33,18 +35,18 @@ class requisitionSystem: # Class to encapsulate methods and parameters for requi
 
   def addRequistion(self): # Method to add a requisition entry
 
-    self.date = input("Enter the date (DD/MM/YEAR): ") # Asks user to enter date
+    global counter
 
-    self.staff_id = input("Enter your 4 Digit Staff ID: ") # Asks user to enter 4 digit staff ID
+    print("----------- Add Requisition Entry -----------")
 
-    self.staff_name = input("Enter your name: ").title # Asks user to enter full name, used title to capitalize the first letter of each section of the name
+    date = input("Enter the date (DD/MM/YEAR): ") # Asks user to enter date
+    staff_id = input("Enter your 4 Digit Staff ID: ") # Asks user to enter 4 digit staff ID
+    staff_name = input("Enter your name: ").title() # Asks user to enter full name, used title to capitalize the first letter of each section of the name
 
-    self.requisition_id = 10000 + counter # Value for the requisition ID starts at 
+    counter += 1
+    requisition_id = counter  # Value for the requisition ID starts at 
 
-    for requisition in self.__requisitions: #Per new requisition, the counter will increment by 1 to allow each requisition ID to be unique
-      counter =+ 1
-
-    self.total = int(input("Enter your total amount the you would like to requisite: $"))
+    details = self.requisitionDetails()
     
     self.approval_reference_number = self.staff_id + self.requisition_id[-3:]
 
